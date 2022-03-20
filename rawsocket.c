878b025daf42d3515327c8e896cf77ed1f1e9b02
Amanda Olearczuk <35851317+AmandaOlearczuk@users.noreply.github.com>
2022-03-20 16:42:56 -0600

    Update rawsocket.c

--- rawsocket.c
+++ rawsocket.c
@@ -56,24 +56,28 @@ int main (void)
 	char *data = datagram + sizeof(struct iphdr) + sizeof(struct tcphdr);
 	strcpy(data , "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
 	
-	//Define IPv4 address
-	struct sockaddr_in ip4_addr;
-	ip4_addr.sin_family = AF_INET;
-	ip4_addr.sin_port = htons(1203); 
-	inet_pton(AF_INET, "136.159.5.25", &ip4_addr.sin_addr);
+	//Define destination IPv4 address
+	struct sockaddr_in ip4_dest_addr;
+	ip4_dest_addr.sin_family = AF_INET;
+	ip4_dest_addr.sin_port = htons(1203); 
+	inet_pton(AF_INET, "136.159.5.25", &ip4_dest_addr.sin_addr);
 	
+	//Fill in ip header fields
+	(*ip_header).version = 4; //Version
 	(*ip_header).ihl = 5; //IHL
-	(*ip_header).version  = 4; //Version
-	(*ip_header).tos  = 0; //Type of Service
+	(*ip_header).tos = 0; //Type of Service
 	(*ip_header).tot_len = sizeof (struct iphdr) + sizeof (struct tcphdr) + strlen(data);
-	(*ip_header).id  = htonl(12345); //Identification
-	(*ip_header).frag_off  = 0; //First fragment has offset 0
-	(*ip_header).ttl  = 128; 
-	(*ip_header).protocol  = IPPROTO_TCP;
-	(*ip_header).check  = 0; //Checksum calculated below
-	//*ip_header.saddr  = inet_addr(source_ip);
+	(*ip_header).id = htonl(12345); //Identification
+	(*ip_header).frag_off = 0; //First fragment has offset 0
+	(*ip_header).ttl = 128; 
+	(*ip_header).protocol = IPPROTO_TCP;
+	(*ip_header).check = csum_tcp((unsigned short *) datagram, (*ip_header).tot_len); //Checksum calculation
+	//*ip_header.saddr = inet_addr(source_ip);
 	inet_pton(AF_INET, "192.168.1.2", &(*ip_header).saddr); //fake source IP
-	(*ip_header).daddr = ip4_addr.sin_addr.s_addr;
+	(*ip_header).daddr = ip4_dest_addr.sin_addr.s_addr; //Destination IP
+	
+	//Fill in the tcp header fields
+	
 	
 	
 	    
