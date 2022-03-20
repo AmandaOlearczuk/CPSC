8a78a0ef6c202e1f3460361f10635132882d4877
Amanda Olearczuk <35851317+AmandaOlearczuk@users.noreply.github.com>
2022-03-20 14:43:18 -0600

    fix print statements (again)

--- rawsocket.c
+++ rawsocket.c
@@ -53,7 +53,7 @@ int main (void)
 	struct iphdr *ip_header = (struct iphdr *) datagram;
 	struct tcphdr *tcp_header = (struct tcphdr *) (datagram + sizeof(struct ip));
 	
-	printf("%d",sizeof(struct ip));
-	printf("%d",sizeof(struct iphdr));
+	printf("%zu",sizeof(struct ip));
+	printf("%zu",sizeof(struct iphdr));
 	    
 }
