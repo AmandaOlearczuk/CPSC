5603706b2bc42e7d5e149c47ac53161f84ea174e
Amanda Olearczuk <35851317+AmandaOlearczuk@users.noreply.github.com>
2022-03-20 13:13:30 -0600

    Create rawsocket.c

---
+++ rawsocket.c
@@ -0,0 +1,24 @@
+#include <stdio.h>	//printf()
+#include <string.h> //memset()
+#include <sys/socket.h>	//socket
+#include <stdlib.h> //exit();
+#include <errno.h> //error number
+#include <netinet/tcp.h>	//Provides declarations for tcp header
+#include <netinet/ip.h>	//Provides declarations for ip header
+#include <arpa/inet.h> // inet_addr
+#include <unistd.h> // sleep()
+
+int main (void)
+{
+	int s = socket (AF_INET, SOCK_RAW, IPPROTO_RAW);
+	
+	if(s == -1)
+	{
+		perror("Socket creation failed");
+		exit(1);
+	}
+	char datagram[4096];
+  char source_ip[32];
+	
+	memset(datagram, 0, sizeof(datagram)); //zero out datagram
+}
